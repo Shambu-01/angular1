c3457106280b6edddd314335cd2dd9aa0f81c9a9
Shambu-01 <jakkula.shambhavi31@gmail.com>
2024-03-01 02:08:14 +0530

    firebase

---
+++ FireBaseAuth/src/environments/environments.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBldaFKx50Uo3XMSKnRf31jVQyUag1o-mo",
+    authDomain: "project1-28f91.firebaseapp.com",
+    projectId: "project1-28f91",
+    storageBucket: "project1-28f91.appspot.com",
+    messagingSenderId: "926335195689",
+    appId: "1:926335195689:web:58107e6ff1429b73749a38"
+  };
+export default firebaseConfig;  
